9e2441a34d54c7238d112cee14c9ae4f021a9352
Corey Farwell <coreyf@rwell.org>
2018-10-13 08:29:04 -0400

    Migrate platforms to traits.

--- main.rs
+++ main.rs
@@ -12,67 +12,6 @@ use std::{borrow, env, error, io, process};
 
 type ExitCode = i32;
 
-#[cfg(target_os = "macos")]
-fn notify(msg_title: &str, msg_body: &str) {
-    let bundle = mac_notification_sys::get_bundle_identifier("Script Editor").unwrap();
-    mac_notification_sys::set_application(&bundle).unwrap();
-    mac_notification_sys::send_notification(msg_title, &None, msg_body, &None).unwrap();
-}
-
-#[cfg(target_os = "linux")]
-fn notify(msg_title: &str, msg_body: &str) {
-    notify_rust::Notification::new()
-        .summary(msg_title)
-        .body(msg_body)
-        .show()
-        .unwrap();
-}
-
-#[cfg(target_os = "windows")]
-fn notify(msg_title: &str, msg_body: &str) {
-    use winrt::windows::data::xml::dom::*;
-    use winrt::windows::ui::notifications::*;
-    use winrt::*;
-    unsafe {
-        let toast_xml =
-            ToastNotificationManager::get_template_content(ToastTemplateType_ToastText02).unwrap();
-        let toast_text_elements = toast_xml
-            .get_elements_by_tag_name(&FastHString::new("text"))
-            .unwrap();
-
-        toast_text_elements
-            .item(0)
-            .unwrap()
-            .append_child(
-                &*toast_xml
-                    .create_text_node(&FastHString::from(msg_title))
-                    .unwrap()
-                    .query_interface::<IXmlNode>()
-                    .unwrap(),
-            )
-            .unwrap();
-        toast_text_elements
-            .item(1)
-            .unwrap()
-            .append_child(
-                &*toast_xml
-                    .create_text_node(&FastHString::from(msg_body))
-                    .unwrap()
-                    .query_interface::<IXmlNode>()
-                    .unwrap(),
-            )
-            .unwrap();
-
-        let toast = ToastNotification::create_toast_notification(&*toast_xml).unwrap();
-        ToastNotificationManager::create_toast_notifier_with_id(&FastHString::new(
-            "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe",
-        ))
-        .unwrap()
-        .show(&*toast)
-        .unwrap();
-    }
-}
-
 fn exit_status_to_message(exit_status: process::ExitStatus) -> borrow::Cow<'static, str> {
     match exit_status.code() {
         Some(0) => "Command exited successfully".into(),
@@ -104,28 +43,129 @@ fn alert_after() -> Result<ExitCode, Box<error::Error>> {
     let mut child = spawn_command(&args)?;
     let exit_status = child.wait().expect("failed to wait on command");
     let cmd_success = exit_status_to_message(exit_status);
-    notify(&args.join(" "), &cmd_success);
+    CurrPlatform::notify(&args.join(" "), &cmd_success);
     Ok(exit_status.code().unwrap_or(0))
 }
 
-#[cfg(windows)]
 fn run() {
-    let rt = winrt::RuntimeContext::init();
+    let mut p = CurrPlatform::setup();
     match alert_after() {
         Ok(exit_code) => process::exit(exit_code),
         Err(e) => writeln!(io::stderr(), "aa: {}", e).expect("could not write to stderr"),
     }
-    rt.uninit();
-}
-
-#[cfg(not(windows))]
-fn run() {
-    match alert_after() {
-        Ok(exit_code) => process::exit(exit_code),
-        Err(e) => writeln!(io::stderr(), "aa: {}", e).expect("could not write to stderr"),
-    }
+    p.teardown();
 }
 
 fn main() {
     run();
 }
+
+trait Platform {
+    fn setup() -> Self;
+    fn notify(msg_title: &str, msg_body: &str);
+    fn teardown(&mut self);
+}
+
+#[cfg(target_os = "windows")]
+struct Windows(winrt::RuntimeContext);
+
+#[cfg(target_os = "windows")]
+impl Platform for Windows {
+    fn setup() -> Self {
+        Windows(winrt::RuntimeContext::init())
+    }
+
+    fn notify(msg_title: &str, msg_body: &str) {
+        use winrt::windows::data::xml::dom::*;
+        use winrt::windows::ui::notifications::*;
+        use winrt::*;
+        unsafe {
+            let toast_xml =
+                ToastNotificationManager::get_template_content(ToastTemplateType_ToastText02)
+                    .unwrap();
+            let toast_text_elements = toast_xml
+                .get_elements_by_tag_name(&FastHString::new("text"))
+                .unwrap();
+
+            toast_text_elements
+                .item(0)
+                .unwrap()
+                .append_child(
+                    &*toast_xml
+                        .create_text_node(&FastHString::from(msg_title))
+                        .unwrap()
+                        .query_interface::<IXmlNode>()
+                        .unwrap(),
+                )
+                .unwrap();
+            toast_text_elements
+                .item(1)
+                .unwrap()
+                .append_child(
+                    &*toast_xml
+                        .create_text_node(&FastHString::from(msg_body))
+                        .unwrap()
+                        .query_interface::<IXmlNode>()
+                        .unwrap(),
+                )
+                .unwrap();
+
+            let toast = ToastNotification::create_toast_notification(&*toast_xml).unwrap();
+            ToastNotificationManager::create_toast_notifier_with_id(&FastHString::new(
+                "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe",
+            ))
+            .unwrap()
+            .show(&*toast)
+            .unwrap();
+        }
+    }
+
+    fn teardown(&mut self) {
+        self.0.uninit();
+    }
+}
+
+#[cfg(target_os = "macos")]
+struct MacOs;
+
+#[cfg(target_os = "macos")]
+impl Platform for MacOs {
+    fn setup() -> Self {
+        MacOs
+    }
+
+    fn notify(msg_title: &str, msg_body: &str) {
+        let bundle = mac_notification_sys::get_bundle_identifier("Script Editor").unwrap();
+        mac_notification_sys::set_application(&bundle).unwrap();
+        mac_notification_sys::send_notification(msg_title, &None, msg_body, &None).unwrap();
+    }
+
+    fn teardown(&mut self) {}
+}
+
+#[cfg(target_os = "linux")]
+struct Linux;
+
+#[cfg(target_os = "linux")]
+impl Platform for Linux {
+    fn setup() -> Self {
+        Linux
+    }
+
+    fn notify(msg_title: &str, msg_body: &str) {
+        notify_rust::Notification::new()
+            .summary(msg_title)
+            .body(msg_body)
+            .show()
+            .unwrap();
+    }
+
+    fn teardown(&mut self) {}
+}
+
+#[cfg(target_os = "windows")]
+type CurrPlatform = Windows;
+#[cfg(target_os = "macos")]
+type CurrPlatform = MacOs;
+#[cfg(target_os = "linux")]
+type CurrPlatform = Linux;
